132aac149c57608a8f820235effe39f9ddb53752
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-02-10 15:04:24 +0100

    signup done

--- routes/myShop.js
+++ routes/myShop.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const productModel = require("../models/product");
 
-router.get('/select-items', function(req, res, next) {
-    productModel.find({isTemplate : true})
-    .then(templates => res.render('sellers/selectItems', {templates, css: ["select-items"]}))
-  });
-  
-=======
-<<<<<<< HEAD
+router.get('/select-items', function (req, res, next) {
+    productModel.find({
+            isTemplate: true
+        })
+        .then(templates => res.render('sellers/selectItems', {
+            templates,
+            css: ["select-items"]
+        }))
+});
+
+
 const sellerModel = require("../models/seller");
 const shopModel = require("../models/shop");
 const bcrypt = require("bcrypt");
@@ -18,9 +22,7 @@ const bcrypt = require("bcrypt");
 router.get("/signup", (req, res) => {
     res.render("sellers/signup");
 });
-=======
-const productModel = require("../models/product")
->>>>>>> 5a9475943ca7a71e14d063e165065cfe97268e74
+
 
 // Get all the products for Shelves
 
@@ -28,73 +30,79 @@ router.get('/myshelves'), (req, res, next) => {
     productModel.find()
         .then(dbRes => {
             // Put all data into an obj called products:
-            res.render('sellers/myShelves',{products: dbRes});
-            console.log(dbRes);  
+            res.render('sellers/myShelves', {
+                products: dbRes
+            });
+            console.log(dbRes);
         })
         .catch(next)
 };
->>>>>>> 8fa441ea7e2eb6f730e65e158842910db329b9e4
-
-
 
 
 router.post("/signup", (req, res, next) => {
-    const newSeller = {username :req.body.username, email: req.body.email, password:req.body.password}
-    const newShop = {name :req.body.name}
-    
+    const newSeller = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    }
+    const newShop = {
+        name: req.body.name
+    }
+
 
     // if (req.file) user.avatar = req.file.secure_url;
-    shopModel 
-    if (!newSeller.username  || !newSeller.username || !user.email || !user.password) {
+    shopModel
+    if (!newSeller.username || !newSeller.username || !newSeller.email || !newSeller.password) {
         res.redirect("/myshop/signup"); // flash
         return;
 
     } else {
 
-        shopModel
-            .findOne({
-                name: newShop.name,
-            })
+        Promise.all([
+                shopModel.findOne({
+                    name: newShop.name,
+                }),
+                sellerModel.findOne({
+                    email: newSeller.email,
+                })
+            ])
             .then(dbRes => {
-<<<<<<< HEAD
-                if (dbRes) return res.redirect("/signup"); //
-
-                shopModel
-                    .create({name:user.name)
-                    .then(() => res.redirect("/signin"))
-=======
-                if (dbRes) {return res.redirect("/signup")} //flash
-                else {
-                    shopModel
-                    .create(newShop)
-                    .then(() => res.redirect("/login"))
-                }
+                if (dbRes[0]) {
+                    console.log("nom déjà pris")
+                    return res.redirect("/signup")
+                } //flash : ce nom est déjà pris
+                if (dbRes[1]) {
+                    console.log(" a déjà un email")
+                    return res.redirect("/signup")
+                } //flash : cet email est déjà affilié à une boutique => pour qu'il se login 
                 
->>>>>>> 042457198cbdc7fc7192101807a3a3c38a9f6c5f
+                    const salt = bcrypt.genSaltSync(10); // https://en.wikipedia.org/wiki/Salt_(cryptography)
+                    const hashed = bcrypt.hashSync(newSeller.password, salt); // generates a secured random hashed password
+                    newSeller.password = hashed // new user is ready for db
+                    Promise.all([
+                        shopModel.create(newShop), sellerModel.create(newSeller)])
+                        .then(() => res.redirect("/login"))
+                }
+            )
+            .catch(next)
+    }})
 
-                // .catch(dbErr => console.log(dbErr));
-            })
-            .catch(next);
-        }
 
-        sellerModel
-        .findOne({
-            email: user.email,
-        })
-        .then(dbRes => {
-            if (dbRes) return res.redirect("/signup");
+//         sellerModel.findOne({email: newSeller.email,})
+//         .then(dbRes => {
+//             if (dbRes) return res.redirect("/signup");
 
-            const salt = bcrypt.genSaltSync(10); // https://en.wikipedia.org/wiki/Salt_(cryptography)
-            const hashed = bcrypt.hashSync(user.password, salt); // generates a secured random hashed password
-            user.password = hashed; // new user is ready for db
+//             const salt = bcrypt.genSaltSync(10); // https://en.wikipedia.org/wiki/Salt_(cryptography)
+//             const hashed = bcrypt.hashSync(user.password, salt); // generates a secured random hashed password
+//             user.password = hashed; // new user is ready for db
 
-            sellerModel
-                .create(user)
-                .then(() => res.redirect("/signin"))
+//             sellerModel
+//                 .create(newSeller)
+//                 .then(() => res.redirect("/login"))
 
-        })
-        .catch(next);
-})
+//         })
+//         .catch(next);
+// })
 
 
 module.exports = router;
