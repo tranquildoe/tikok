c66ada84eb79668e22651d3564ac59b824fc10aa
camilledlr <robinel.camille@gmail.com>
2020-02-10 15:55:36 +0100

    created createShop

--- routes/myShop.js
+++ routes/myShop.js
@@ -1,28 +1,20 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const productModel = require("../models/product");
 
-router.get('/select-items', function(req, res, next) {
-    productModel.find({isTemplate : true})
-    .then(templates => res.render('sellers/selectItems', {templates, css: ["select-items"]}))
-  });
-  
-=======
-<<<<<<< HEAD
 const sellerModel = require("../models/seller");
 const shopModel = require("../models/shop");
 const bcrypt = require("bcrypt");
 
-router.get("/signup", (req, res) => {
-    res.render("sellers/signup");
-});
-=======
-const productModel = require("../models/product")
->>>>>>> 5a9475943ca7a71e14d063e165065cfe97268e74
+// to add in the  sign up part : .then(createdShop => res.redirect(`/createShop/${createdShop}`))
 
 // Get all the products for Shelves
+router.get('/createShop/:id', (req, res, next) => {
+    shopModel.findById(req.params.id)
+    .then( shop => res.render('/sellers/createItem', {shop}))
+})
 
 router.get('/myshelves'), (req, res, next) => {
     productModel.find()
@@ -33,60 +25,17 @@ router.get('/myshelves'), (req, res, next) => {
         })
         .catch(next)
 };
->>>>>>> 8fa441ea7e2eb6f730e65e158842910db329b9e4
 
 
 
+router.get('/select-items', function(req, res, next) {
+    productModel.find({isTemplate : true, })
+    .then(templates => res.render('sellers/selectItems', {templates, css: ["select-items"]}))
+  });
 
-router.post("/signup", (req, res, next) => {
-    const newSeller = {username :req.body.username, email: req.body.email, password:req.body.password}
-    const newShop = {name :req.body.name}
-    
-
-    // if (req.file) user.avatar = req.file.secure_url;
-    shopModel 
-    if (!newSeller.username  || !newSeller.username || !user.email || !user.password) {
-        res.redirect("/myshop/signup"); // flash
-        return;
-
-    } else {
-
-        shopModel
-            .findOne({
-                name: newShop.name,
-            })
-            .then(dbRes => {
-                if (dbRes) {return res.redirect("/signup")} //flash
-                else {
-                    shopModel
-                    .create(newShop)
-                    .then(() => res.redirect("/login"))
-                }
-                
-
-                // .catch(dbErr => console.log(dbErr));
-            })
-            .catch(next);
-        }
-
-        sellerModel
-        .findOne({
-            email: user.email,
-        })
-        .then(dbRes => {
-            if (dbRes) return res.redirect("/signup");
-
-            const salt = bcrypt.genSaltSync(10); // https://en.wikipedia.org/wiki/Salt_(cryptography)
-            const hashed = bcrypt.hashSync(user.password, salt); // generates a secured random hashed password
-            user.password = hashed; // new user is ready for db
-
-            sellerModel
-                .create(user)
-                .then(() => res.redirect("/signin"))
-
-        })
-        .catch(next);
-})
-
+router.post('/select-items', function(req, res, next) {
+    console.log(req.body)
+  });
+  
 
 module.exports = router;
