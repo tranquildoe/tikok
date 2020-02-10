7d72c5bc6288ed84477e46b2444bdc033d9903f4
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-02-10 16:17:18 +0100

    currentUser issue

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 var indexRouter = require('./routes/index');
 var custRouter = require('./routes/customers');
@@ -26,11 +28,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/shopping', custRouter);
 app.use('/admin', adminRouter);
-app.use('/auth', authRouter);
+app.use('/', authRouter);
 app.use('/myshop', shopRouter);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 60000 }, // in millisec
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    saveUninitialized: true,
+    resave: true
+  })
+);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/myShop.js
+++ routes/myShop.js
@@ -1,29 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const mongoose = require("mongoose");
-const productModel = require("../models/product");
-
-router.get('/select-items', function (req, res, next) {
-    productModel.find({
-            isTemplate: true
-        })
-        .then(templates => res.render('sellers/selectItems', {
-            templates,
-            css: ["select-items"]
-        }))
-});
-
-
-const sellerModel = require("../models/seller");
-const shopModel = require("../models/shop");
-const bcrypt = require("bcrypt");
-
-router.get("/signup", (req, res) => {
-    res.render("sellers/signup");
-});
-
-
 // Get all the products for Shelves
 
 router.get('/myshelves'), (req, res, next) => {
@@ -37,72 +14,6 @@ router.get('/myshelves'), (req, res, next) => {
         })
         .catch(next)
 };
-
-
-router.post("/signup", (req, res, next) => {
-    const newSeller = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    }
-    const newShop = {
-        name: req.body.name
-    }
-
-
-    // if (req.file) user.avatar = req.file.secure_url;
-    shopModel
-    if (!newSeller.username || !newSeller.username || !newSeller.email || !newSeller.password) {
-        res.redirect("/myshop/signup"); // flash
-        return;
-
-    } else {
-
-        Promise.all([
-                shopModel.findOne({
-                    name: newShop.name,
-                }),
-                sellerModel.findOne({
-                    email: newSeller.email,
-                })
-            ])
-            .then(dbRes => {
-                if (dbRes[0]) {
-                    console.log("nom déjà pris")
-                    return res.redirect("/signup")
-                } //flash : ce nom est déjà pris
-                if (dbRes[1]) {
-                    console.log(" a déjà un email")
-                    return res.redirect("/signup")
-                } //flash : cet email est déjà affilié à une boutique => pour qu'il se login 
-                
-                    const salt = bcrypt.genSaltSync(10); // https://en.wikipedia.org/wiki/Salt_(cryptography)
-                    const hashed = bcrypt.hashSync(newSeller.password, salt); // generates a secured random hashed password
-                    newSeller.password = hashed // new user is ready for db
-                    Promise.all([
-                        shopModel.create(newShop), sellerModel.create(newSeller)])
-                        .then(() => res.redirect("/login"))
-                }
-            )
-            .catch(next)
-    }})
-
-
-//         sellerModel.findOne({email: newSeller.email,})
-//         .then(dbRes => {
-//             if (dbRes) return res.redirect("/signup");
-
-//             const salt = bcrypt.genSaltSync(10); // https://en.wikipedia.org/wiki/Salt_(cryptography)
-//             const hashed = bcrypt.hashSync(user.password, salt); // generates a secured random hashed password
-//             user.password = hashed; // new user is ready for db
-
-//             sellerModel
-//                 .create(newSeller)
-//                 .then(() => res.redirect("/login"))
-
-//         })
-//         .catch(next);
-// })
 
 
 module.exports = router;
