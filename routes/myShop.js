222722fd172f736e12dd910e6f133f404f01bd3b
camilledlr <robinel.camille@gmail.com>
2020-02-10 13:29:06 +0100

    select item added

--- routes/myShop.js
+++ routes/myShop.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require("mongoose");
+const productModel = require("../models/product");
 
+router.get('/select-items', function(req, res, next) {
+    productModel.find({isTemplate : true})
+    .then(templates => res.render('sellers/selectItems', {templates, css: ["select-items"]}))
+  });
+  
 
 module.exports = router;
