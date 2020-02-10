42433d98b3e4b5d01b165a1c869c6da4ba12c5d4
Shannon Kimmel <ciervatranquila@gmail.com>
2020-02-10 12:48:14 +0100

    myshelves route

--- routes/myShop.js
+++ routes/myShop.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const productModel = require("../models/product")
 
+// Get all the products for Shelves
+
+router.get('/myshelves'), (req, res, next) => {
+    productModel.find()
+        .then(dbRes => {
+            // Put all data into an obj called products:
+            res.render('sellers/myShelves',{products: dbRes});
+            console.log(dbRes);  
+        })
+        .catch(next)
+};
 
 module.exports = router;
