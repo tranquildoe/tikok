c11c5dcbcf908589cffd2735176de1110fe20984
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-02-12 15:12:56 +0100

    aff

--- routes/customers.js
+++ routes/customers.js
@@ -8,17 +8,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get("/platform/:shop_id", (req, res, next) => {
+router.get("/:shop_id", (req, res, next) => {
     shopModel.findById(req.params.shop_id).populate("list_products")
     .then(shop => {
       res.render("platform/shop", {shop})})
     .catch(next)
 })
 
-router.get("plaform/:shop_id/:id" , (req,res,next) => {
+router.get("/:shop_id/:id" , (req,res,next) => {
     productModel.findById(req.params.id)
-    .then(shop => {
-      res.render("platform/product" , {shop})})
+    .then(product => {
+      res.render("platform/product" , {product})})
       .catch(next)
 })
 
