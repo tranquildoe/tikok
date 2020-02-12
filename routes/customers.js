dce7f9fd5e3b15e5be23c6d9be5b17b79b58c131
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-02-12 16:47:31 +0100

    yes

--- routes/customers.js
+++ routes/customers.js
@@ -4,23 +4,34 @@ const productModel = require("../models/product");
 const shopModel = require("../models/shop");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get("/:shop_id", (req, res, next) => {
-    shopModel.findById(req.params.shop_id).populate("list_products")
+router.get("/shop/:shop_id", (req, res, next) => {
+  shopModel.findById(req.params.shop_id).populate("list_products")
     .then(shop => {
-      res.render("platform/shop", {shop})})
+      res.render("platform/shop", {
+        shop
+      })
+    })
     .catch(next)
 })
 
-router.get("/:shop_id/:id" , (req,res,next) => {
-    productModel.findById(req.params.id)
-    .then(product => {
-      res.render("platform/product" , {product})})
-      .catch(next)
+router.get("/:shop_id/:id", (req, res, next) => {
+  Promise.all([
+      shopModel.findById(req.params.shop_id).limit(4).populate("list_products"),
+      productModel.findById(req.params.id)
+    ])
+    .then(dbRes => {
+      res.render("platform/product", {
+          shop: dbRes[0],
+          product: dbRes[1],
+        })
+        .catch(next)
+    })
 })
+
 
 
 
