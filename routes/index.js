584cc4a5d54841d090883740a233d2acef6d259a
camilledlr <robinel.camille@gmail.com>
2020-02-13 09:17:20 +0100

    home page search

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,14 @@ const productModel = require("../models/product");
 const sellerModel = require("../models/seller");
 const shopModel = require("../models/shop");
 
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
 router.get('/home', function(req, res, next) {
   shopModel.find().limit(6)
-  .then(sixShops => res.render('platform/home', {sixShops, css:["home"]}))
+  .then(sixShops => res.render('platform/home', {sixShops, css:["home"], scripts:['home']}))
 });
-
-
 
 function uniquifyArray(array) {
   for (let i = array.length - 1; i >= 0; i--) {
@@ -27,55 +23,18 @@ function uniquifyArray(array) {
   return array;
 }
 
+
+router.get('/shopping/search', function(req, res, next) {
+  console.log(req.query)
+productModel
+.find({name: { $regex: req.query.q, $options: "i" }, isTemplate : false}).populate('id_shop')
+.then(dbRes => res.json(dbRes))
+.catch(next)
+});
+
 router.get('/shopping/:cat', function(req, res, next) {
-productModel.find({category : req.params.cat, isTemplate: false}).populate('id_shop')
-.then(products => res.render('platform/category', {products}))
-});
-
-router.get('/shop/:id/product/:ref', function(req, res, next) {
-// req.params.id // can we put 2 : in the url ?
-  res.render('platform/product');
-});
-
-router.get('/search', function(req, res, next) {
-// req.params.id // can we put 2 : in the url ?
-  res.render('platform/search');
-});
-
-// // get all the collections and Tags
-// router.get('/sneakers/collection', (req, res, next) => {
-//   Promise.all([
-//     sneakersModel.find(),
-//     tagModel.find()
-//   ])
-//       .then(dbRes => {
-//           res.render('products', {
-//               sneakers: dbRes[0],
-//               tags: dbRes[1],
-//           });
-//           console.log(dbRes[0]);
-//       })
-//       .catch(next)
-// });
-// router.get('/sneakers/:cat', (req, res, next) => {
-//   Promise.all([
-//     // The following line: :cat above makes all after : in the url equal to cat var.
-//     // req.params.cat accesses that var
-//     sneakersModel.find({category: req.params.cat}),
-//     tagModel.find()
-//   ])
-//       .then(dbRes => {
-//           res.render('products', {
-//               sneakers: dbRes[0],
-//               tags: dbRes[1],
-//           });
-//           console.log(dbRes[0]);
-//       })
-//       .catch(next)
-// });
-
-
-
-
+  productModel.find({category : req.params.cat, isTemplate: false}).populate('id_shop')
+  .then(products => res.render('platform/category', {products}))
+  });
 
 module.exports = router;
